8ffa99608f0902e890eae0a66a3238156ee23943
Timothy Borovkov <timofei.borovkov@icloud.com>
2016-02-03 21:49:18 +0200

    fix

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
-var app = require('express')();
-var http = require('http').Server(app);
+// Load the http module to create an http server.
+var http = require('http');
 
-app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+// Configure our HTTP server to respond with Hello World to all requests.
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("Hello World\n");
 });
 
-http.listen(8080, function(){
-  console.log('listening on *:8080');
-});
+// Listen on port 8000, IP defaults to 127.0.0.1
+server.listen(8000);
+
+// Put a friendly message on the terminal
+console.log("Server running at http://127.0.0.1:8000/");
