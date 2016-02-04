dad356c8719301a1434fb92106f11bfa71f031df
Timothy Borovkov <timofei.borovkov@icloud.com>
2016-02-04 18:25:06 +0200

    test

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ var port = process.env.PORT || 1337;
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
-app.get('/test/', function (req, res) {
-  res.send('Hello World test!');
+app.get('/test', function (req, res) {
+  res.send('Hello!\n');
+  res.send(req);
 });
 app.listen(port, function () {
   console.log('Example app listening on port ' + port);
