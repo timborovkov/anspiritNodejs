32e7bd6fb2f62a1222e865e01a59f81b6032ba6f
Timothy Borovkov <timofei.borovkov@icloud.com>
2016-02-04 18:18:00 +0200

    express js

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-var http = require('http')
+var express = require('express')
+var app = express()
 var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-    var body = [];
-    req.on('data', function(chunk) {
-      body.push(chunk);
-    }).on('end', function() {
-      body = Buffer.concat(body).toString();
-      response.end(body);
-    });
-}).listen(port);
+
+app.get('/', function (req, res) {
+  res.send('Hello World from express');
+})
+
+app.listen(port);
