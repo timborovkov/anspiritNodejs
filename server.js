0b0dac751016b5eaf1f8a5b614b91c51cc58de2e
Timothy Borovkov <timofei.borovkov@icloud.com>
2016-02-04 18:55:58 +0200

    cleanup

--- server.js
+++ server.js
@@ -2,6 +2,23 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 1337;
 
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+/*
+app.get('/login', function (req, res) {
+  var login = require('/api/login.js').run();
+  if(login){
+    res.send('Done login');
+  }else{
+    res.send('Failed to login');
+  }
+});
+*/
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port);
+});
+
 //database
 var mysql = require('mysql');
 var db = mysql.createConnection({
@@ -11,18 +28,3 @@ var db = mysql.createConnection({
   database : 'anspiritMain'
 });
 db.connect();
-
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-app.get('/login', function (req, res) {
-  var login = require('/api/login.js').run();
-  if(login){
-    res.send('Done login');
-  }else{
-    res.send('Failed to login');
-  }
-});
-app.listen(port, function () {
-  console.log('Example app listening on port ' + port);
-});
