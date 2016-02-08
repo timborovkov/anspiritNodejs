85e976c36ed52215c5d4543b4560e9718234e7f8
Timothy Borovkov <timofei.borovkov@icloud.com>
2016-02-08 14:01:41 +0200

    test

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ var http = require('http').Server(app);
 app.get('/', function(req, res){
   res.send('<h1>Hello world</h1>');
 });
+app.get('/login', function(req, res){
+  res.send('<h1>Login</h1>');
+});
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
