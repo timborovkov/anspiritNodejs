3c20939eb4f892e771ea70dde595d7baefa03cba
Timothy Borovkov <timofei.borovkov@icloud.com>
2016-02-08 14:00:28 +0200

    test

--- server.js
+++ server.js
@@ -1,37 +1,10 @@
-var express = require('express');
-var app = express();
-var port = process.env.PORT || 1337;
+var app = require('express')();
+var http = require('http').Server(app);
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/', function(req, res){
+  res.send('<h1>Hello world</h1>');
 });
 
-app.listen(port, function () {
-  console.log('Example app listening on port ' + port);
+http.listen(3000, function(){
+  console.log('listening on *:3000');
 });
-
-/*
-//database
-var mysql = require('mysql');
-var db = mysql.createConnection({
-  host     : 'eu-cdbr-azure-north-d.cloudapp.net',
-  user     : 'b2a32c755154bf',
-  password : 'c0b4e78d',
-  database : 'anspiritMain'
-});
-db.connect();
-
-app.post('/login', function (req, res) {
-  var login = require('api/login.js');
-  var in = login.run(req);
-  if(login != null){
-    if(in){
-      res.send('Done login');
-    }else{
-      res.send('Failed to login');
-    }
-  }else{
-    res.send('Login failed: server error');
-  }
-});
-*/
